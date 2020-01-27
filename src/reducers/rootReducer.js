498c1b44a7ac87c5a0f0bffb054adafddb0ae4b6
Amit Kumar <akamit21@outlook.com>
2020-01-28 01:01:33 +0530

    updated

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
+import authReducer from "./authReducer";
 import transactionReducer from "./transactionReducer";
 import accountReducer from "./accountReducer";
 
 const rootReducer = combineReducers({
+  authReducer,
   transactionReducer,
   accountReducer
 });
